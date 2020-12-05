c6c691fe2b4d83822b3ce8b1989b251962c23115
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2020-12-05 17:16:35 -0500

    Refactor to use promises

--- background.js
+++ background.js
@@ -1,96 +1,92 @@
-const notificationsUrl = "https://github.com/notifications";
-var count = 0;
+const notificationsUrl = 'https://github.com/notifications';
+let count = 0;
 
-function update() {
-    browser.storage.local
-        .get({
+async function update() {
+    let options,
+        notifications;
+
+    try {
+        options = await browser.storage.local.get({
             accessToken: '',
             showNotifications: false,
             showNotificationsDecreased: true
-        })
-        .then((options) => {
-            if (!options.accessToken) {
-                return;
-            }
-
-            fetch('https://api.github.com/notifications', {
-                headers: {'Authorization': `token ${options.accessToken}`},
-                cache: 'reload'
-            })
-                .then((response) => {
-                    if (response.ok) {
-                        return response.json();
-                    }
-
-                    response.json().then((error) => {
-                        browser.browserAction.setTitle({
-                            title: error.message
-                        });
-                    });
-
-                    throw new Error(response.statusText);
-                })
-                .then((notifications) => {
-                    browser.browserAction.setTitle({
-                        title: 'GitHub Notifier'
-                    });
-                    browser.browserAction.enable();
-
-                    if (0 === notifications.length) {
-                        browser.browserAction.setBadgeText({
-                            text: ''
-                        });
-
-                        return;
-                    }
-
-                    let badgeText = notifications.length
-                        + (notifications.length >= 50 ? '+' : '');
-
-                    browser.browserAction.setBadgeText({
-                        text: badgeText
-                    });
-
-                    if (count === notifications.length) {
-                        return;
-                    }
-
-                    if (count > notifications.length && !options.showNotificationsDecreased) {
-                        return;
-                    }
-
-                    count = notifications.length;
-
-                    if (!options.showNotifications) {
-                        return;
-                    }
-
-                    if (!count) {
-                        return;
-                    }
-
-                    let items = '';
-
-                    notifications.forEach((notification) => {
-                        items += `[${notification.repository.full_name}] ${notification.subject.title} \n`;
-                    });
-
-                    browser.notifications.create({
-                        type: "basic",
-                        iconUrl: browser.extension.getURL("icons/github.svg"),
-                        title: count === 1
-                            ? `There is 1 new notification`
-                            : `There are ${badgeText} new notifications`,
-                        message: items
-                    });
-                })
-                .catch((error) => {
-                    browser.browserAction.setTitle({
-                        title: error.message
-                    });
-                    browser.browserAction.disable();
-                });
         });
+
+        if (0 === options.accessToken.length) {
+            throw new Error('Access Token not found.');
+        }
+
+        let response = await fetch('https://api.github.com/notifications', {
+            headers: {'Authorization': `token ${options.accessToken}`},
+            cache: 'reload'
+        });
+
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+
+        notifications = await response.json();
+    } catch (error) {
+        browser.browserAction.setTitle({
+            title: error.message
+        });
+        browser.browserAction.disable();
+
+        return;
+    }
+
+    browser.browserAction.setTitle({
+        title: 'GitHub Notifier'
+    });
+    browser.browserAction.enable();
+
+    if (0 === notifications.length) {
+        browser.browserAction.setBadgeText({
+            text: ''
+        });
+
+        return;
+    }
+
+    let badgeText = notifications.length
+        + (notifications.length >= 50 ? '+' : '');
+
+    browser.browserAction.setBadgeText({
+        text: badgeText
+    });
+
+    if (count === notifications.length) {
+        return;
+    }
+
+    if (count > notifications.length && !options.showNotificationsDecreased) {
+        return;
+    }
+
+    count = notifications.length;
+
+    if (!options.showNotifications) {
+        return;
+    }
+
+    if (!count) {
+        return;
+    }
+
+    let items = '';
+
+    notifications.forEach((notification) => {
+        items += `[${notification.repository.full_name}] ${notification.subject.title} \n`;
+    });
+
+    browser.notifications.create({
+        type: "basic",
+        iconUrl: browser.extension.getURL("icons/github.svg"),
+        title: count === 1
+            ? `There is 1 new notification`
+            : `There are ${badgeText} new notifications`,
+        message: items
+    });
 }
 
 setInterval(update, 1000 * 60);
